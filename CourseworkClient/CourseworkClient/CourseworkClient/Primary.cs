cff4b46a608ba41d9cd52a719bab46d1e3337711
Elliot <es13890@reigate.ac.uk>
2016-11-10 10:37:08 +0000

    Added DB for server and added hashing algorithm for client

---
+++ CourseworkClient/CourseworkClient/CourseworkClient/Primary.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CourseworkClient
+{
+
+    public class Primary : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        HMACMD5 hasher = new HMACMD5();
+        static void Main(string[] args)
+        {
+            using (Primary game = new Primary())
+            {
+                game.Run();
+            }
+        }
+        public Primary()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.IsFullScreen = true;
+
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            Window.Title = this.ComputeHash(new Random().Next().ToString());
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Bisque);
+
+            base.Draw(gameTime);
+        }
+        public string ComputeHash(string s)
+        {
+            byte[] b = hasher.ComputeHash(Encoding.UTF8.GetBytes(s));
+            string output = "";
+            foreach (byte e in b)
+            {
+                output += e.ToString("x0");
+            }
+            return output;
+        }
+        
+    }
+}
