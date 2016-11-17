4597966437113a9848a4c08aa287eeea8e522e27
Elliot <es13890@reigate.ac.uk>
2016-11-17 09:15:54 +0000

    WEWEWQ

---
+++ Coursework Server/DatabaseHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CourseworkServer
+{
+    class DatabaseHandler
+    {
+        public bool CheckLoginCredentials(string username, string passwordHash)
+        {
+            //checking code goes in here
+            return true; //Remove this later
+        }
+    }
+}
