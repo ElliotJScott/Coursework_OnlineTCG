77651c6e3fc1cd5440e997d73fb7d83d60e0cd25
Elliot Scott <elliotscott99@hotmail.co.uk>
2016-12-14 22:25:39 +0000

    F

---
+++ CourseworkClient/CourseworkClient/CourseworkClient/CardBuilder.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CourseworkClient
+{
+    class CardBuilder
+    {
+        const int cardArtDispX = 10;
+        const int cardArtDispY = 20;
+        public static void DrawCard(Card c, Vector2 pos, bool big, SpriteBatch sb) //Note this doesn't include the inner text which only applies to the big cards
+        {
+            #region Inner and Outer Texture Setting
+            Texture2D innerTexture = null;
+            Texture2D cardOutline;
+            if (big)
+            {
+                cardOutline = Primary.game.cardOutlineBig;
+                switch (c.type)
+                {
+                    case 0:
+                        innerTexture = Primary.game.unitBigInner;
+                        break;
+                    case 1:
+                        innerTexture = Primary.game.techBigInner;
+                        break;
+                    case 2:
+                        innerTexture = Primary.game.upgradeBigInner;
+                        break;
+                }
+            }
+            else
+            {
+                cardOutline = Primary.game.cardOutlineSmall;
+                switch (c.type)
+                {
+                    case 0:
+                        innerTexture = Primary.game.unitSmallInner;
+                        break;
+                    case 1:
+                        innerTexture = Primary.game.techSmallInner;
+                        break;
+                    case 2:
+                        innerTexture = Primary.game.upgradeSmallInner;
+                        break;
+
+                }
+            }
+            #endregion
+            sb.Draw(cardOutline, new Rectangle((int)pos.X, (int)pos.Y, cardOutline.Width, cardOutline.Height), Color.White);
+            sb.Draw(innerTexture, new Rectangle((int)pos.X, (int)pos.Y, innerTexture.Width, innerTexture.Height), Color.White);
+            int disp = big ? 10 : 5;
+            Texture2D cardArt = Primary.game.GetCardArt(c.name);
+            sb.Draw(cardArt, new Rectangle(big ? cardArtDispX : cardArtDispX / 2, big ? cardArtDispY : cardArtDispY / 2, big ? cardArt.Width : cardArt.Width / 2, big ? cardArt.Height : cardArt.Height / 2), Color.White);
+            sb.DrawString(Primary.game.mainFont, c.name, pos + new Vector2(disp, disp), Color.Black);
+        }
+    }
+}
---
+++ CourseworkClient/CourseworkClient/CourseworkClient/Gui/BigCard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace CourseworkClient.Gui
+{
+    enum Location
+    {
+        InHand,
+        InPlay,
+    }
+    class BigCard : GuiItem
+    {
+        Card card;
+        public BigCard(Card c)
+        {
+            card = c;
+            Viewport v = Primary.game.GraphicsDevice.Viewport;
+            subItems.Add(new CardText(Primary.game.mainFont, new Rectangle(10 + (v.Width - Primary.game.cardOutlineBig.Width) / 2, 100 + (v.Height - Primary.game.cardOutlineBig.Height) / 2, 100, 100), c.getEffectNames(), 10, 10, c.getColourList()));
+        }
+        public override void Draw(SpriteBatch sb)
+        {
+            Viewport v = Primary.game.GraphicsDevice.Viewport;
+            CardBuilder.DrawCard(card, new Vector2((v.Width - Primary.game.cardOutlineBig.Width) / 2, (v.Height - Primary.game.cardOutlineBig.Height) / 2), true, sb);
+            foreach (GuiItem g in subItems) g.Draw(sb);
+        }
+
+        public override void Update()
+        {
+            MouseState m = Mouse.GetState();
+            Vector2 mp = new Vector2(m.X, m.Y);
+            if (Clicked(mp) || DeClicked(mp))
+            {
+                foreach (GuiItem g in Primary.game.currentForm.formItems)
+                {
+                    if (g.GetType() == typeof(SmallCard))
+                    {
+                        ((SmallCard)g).drawnBig = false;
+                    }
+                }
+            }
+        }
+    }
+}
